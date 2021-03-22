bc260cad1b1fcfe9e7bf4b75d01f50a4bf1c922a
Germán A. Gómez <germang08@hotmail.com>
2021-03-22 18:25:34 -0300

    remote mysql databe

--- config/config.php
+++ config/config.php
@@ -1,11 +1,20 @@
 <?php
 
-    define('URL', 'http://127.1.1.1/stampymailapp/');
-
+    // local 
+    /* define('URL', 'http://127.1.1.1/stampymailapp/');
     define('HOST', 'localhost');
     define('DB', 'stampymailapp');
     define('USER', 'root');
     define('PASSWORD', '');
+    define('CHARSET', 'utf8mb4'); */
+
+
+    // production
+    define('URL', 'https://stampymailapp.herokuapp.com/');
+    define('HOST', 'remotemysql.com:3306');
+    define('DB', 'QHjdYpSCvW');
+    define('USER', 'QHjdYpSCvW');
+    define('PASSWORD', 'jy3VLLuOGv');
     define('CHARSET', 'utf8mb4');
 
 ?>

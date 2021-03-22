ff1acc59246552abe38bff971d2ceeea1d10527f
Germán A. Gómez <germang08@hotmail.com>
2021-03-22 19:00:51 -0300

    config config.php

--- config/config.php
+++ config/config.php
@@ -1,20 +1,16 @@
 <?php
 
-    // local 
-    /* define('URL', 'http://127.1.1.1/stampymailapp/');
-    define('HOST', 'localhost');
-    define('DB', 'stampymailapp');
-    define('USER', 'root');
-    define('PASSWORD', '');
-    define('CHARSET', 'utf8mb4'); */
+if ($_SERVER['HTTP_HOST'] === 'stampymailapp.herokuapp.com')
+    define('URL', 'https://stampymailapp.herokuapp.com/'); // production
+else
+    define('URL', 'http://127.1.1.1/stampymailapp/');  // development
+
+define('HOST', 'remotemysql.com:3306');
+define('DB', 'QHjdYpSCvW');
+define('USER', 'QHjdYpSCvW');
+define('PASSWORD', 'jy3VLLuOGv');
+define('CHARSET', 'utf8mb4');
 
 
-    // production
-    define('URL', 'https://stampymailapp.herokuapp.com/');
-    define('HOST', 'remotemysql.com:3306');
-    define('DB', 'QHjdYpSCvW');
-    define('USER', 'QHjdYpSCvW');
-    define('PASSWORD', 'jy3VLLuOGv');
-    define('CHARSET', 'utf8mb4');
 
 ?>

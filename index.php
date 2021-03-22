eab9eb11c0043f60c4912298c9d66b93d65f6609
Germán A. Gómez <germang08@hotmail.com>
2021-03-22 19:08:57 -0300

    delete comments

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-/* error_reporting(E_ALL); // Error/Exception engine, always use E_ALL
-ini_set('ignore_repeated_errors', TRUE); // always use TRUE
-ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
-ini_set('log_errors', TRUE); // Error/Exception file logging engine.
-ini_set("error_log", "X:\www\stampymailapp\php-error.log"); */
-
-
 require_once 'libs/database.php';
 require_once 'helpers/encryption.php';
 require_once 'libs/controller.php';
@@ -15,6 +8,5 @@ require_once 'libs/model.php';
 require_once 'helpers/session/session.php';
 require_once 'libs/app.php';
 require_once 'config/config.php';
-
 
 $app = new App(); 
